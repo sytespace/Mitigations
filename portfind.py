ac54ea8bc693b7031368c19677ab7ad44692927c
itsmmgamer <mauromartins@email.com>
2019-07-11 16:50:21 +0100

    Added port finder

---
+++ portfind.py
@@ -0,0 +1,26 @@
+# portfind.py made by Mauro M.
+
+
+# This code is protected and released to the public under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0) License 
+# witch can be viewed on the Creative Commons website (https://creativecommons.org/licenses/by-nc-nd/4.0/).
+# Any faliure to comply with the terms stipulated in the license will be met with swift legal action 
+# by the Creator.
+
+
+import glob, os, json # Imports
+
+looking = input("What port do you need to find? ")
+
+# ONLY run inside /srv/daemon/config/servers 
+
+files = glob.glob("*/server.json") # Get all the files directorys as list
+
+for x in files: # For each file
+    with open(x, "r") as read_file: # Using itenerable open and preform a set of operations for each file
+        data = json.load(read_file) # Load file as JSON
+        port = data['build']['default']['port'] # Get the value of build/default/port inside the JSON structure
+        if port == input: 
+            uiid = data['uuid'] # Get the uuid
+            print("[SUCESS] Found! Folder is /{}/server.json", uiid) 
+        else:
+            print("[FAIL] Couldn't find any servers with the port {}", port)
